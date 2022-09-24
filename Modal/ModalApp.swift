5d55167a88e4fc0df062c6d9dae3c14110b240f7
Alexandre Azevedo <alexndreazevedo@gmail.com>
2022-09-24 23:49:13 +0200

    Initial Commit

---
+++ Modal/ModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalApp.swift
+//  Modal
+//
+//  Created by Alexandre Azevedo on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
